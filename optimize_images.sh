37e7539762729e32618758b377008890c63e3c66
Chris Hunter <87100912+chris-hunter206@users.noreply.github.com>
2021-12-07 17:01:19 -0800

    added lots of comments

--- optimize_images.sh
+++ optimize_images.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-# create web-optimized resized images in the current directory.
+# Author: Christopher Hunter
 
-# set width for thumbs and medium images
+# This create web-optimized resized images in the current directory.
+# This requires the 'sips' command-line tool which is native on MacOS.
+
+# If you re-run the tool make sure and run 'rm *thumb.jpg; rm *med.jpg'
+# to clear out the resized files. Otherwise you will end up with a huge
+# mess of files. I chose not to enable this by default, as it carries
+# some risk of losing files.
+
+# Uncomment this to remove resized images every time:
+# rm *thumb.jpg; rm *med.jpg
+
+# set width for thumbs and medium images. These are good for landscape.
+# for portrait-oriented images, use 200 for thumbs and 1000 for med, or
+# the images will be a little too blurry.
 THUMB_WIDTH="140"
 MED_WIDTH="850"
 
